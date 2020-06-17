e9ec04e36f5d5f06dab6ecd3ce946b6bcc52e4e5
Matheus Soares <matheus.http@hotmail.com>
2020-06-17 19:58:39 -0300

    new update

--- Assets/Scripts/Raycaster.cs
+++ Assets/Scripts/Raycaster.cs
@@ -7,12 +7,15 @@ public class Raycaster : MonoBehaviour
 {
     public TextMesh textDebug;
     public GameObject crosshair;
-    float counter=2;
+    float counter=3;
     public FPSWalk fpswalk;
+    public GameObject[] load = new GameObject[8];
+
+    bool slotKey = false;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -37,39 +40,50 @@ public class Raycaster : MonoBehaviour
             if (hit.transform.gameObject.CompareTag("Player"))
             {
                 //troca cor do crosshair
-                crosshair.GetComponent<Image>().CrossFadeColor(Color.green, .5f, false, false);
+                //crosshair.GetComponent<Image>().CrossFadeColor(Color.green, .5f, false, false);
+                
                 //decrementa o contador 
                 counter -= Time.deltaTime;
                 //se o contador for < 0 chama a funça no objeto ButtonAction()
                 if (counter < 0)
                 {
                     hit.transform.gameObject.SendMessageUpwards("ButtonAction");
-                    counter = 3;//reseta o contador
+                    counter = 2;//reseta o contador
                 }
             } // senao verifica se o objeto é com o tag andavel
             else if(hit.transform.gameObject.CompareTag("Walkable"))
             {
-                crosshair.GetComponent<Image>().CrossFadeColor(Color.blue, .5f, false, false);
+                //crosshair.GetComponent<Image>().CrossFadeColor(Color.blue, .5f, false, false);
+                hit.transform.GetComponent<SpriteRenderer>().color = new Color(1,1,1,.7f); ///Cor do pépé
                 counter -= Time.deltaTime;
                 if (counter < 0)
                 {
                     //anda com o personagem até o ponto de caminhada
                     fpswalk.positionToGo = hit.transform.position;
-                    counter = 3;//reseta o contador
+                    counter = 2;//reseta o contador
+                }
+            }
+            else if (hit.transform.gameObject.CompareTag("Key"))
+            {
+                counter -= Time.deltaTime;
+                if (counter < 0)
+                {
+                    slotKey = true;
+                    counter = 2;//reseta o contador
                 }
             }
             else
             {
                 //se nao for nada disso reseta o contador
-                counter = 3;
+                counter = 2;
                 //pinta o crossrair de vermelho
-                crosshair.GetComponent<Image>().CrossFadeColor(Color.red, .5f, false, false);
+                //crosshair.GetComponent<Image>().CrossFadeColor(Color.red, .5f, false, false);
             }
         }
         else
         {
             //se nao da raycast o crosshair some
-            crosshair.GetComponent<Image>().CrossFadeColor(Color.black, .0f, false, false);
+            //crosshair.GetComponent<Image>().CrossFadeColor(Color.black, .0f, false, false);
         }
 
        
